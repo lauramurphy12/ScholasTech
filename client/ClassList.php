7b340319d5932552186a2a593d12dcfff50ac05b
lauramurphy12 <55330536+lauramurphy12@users.noreply.github.com>
2021-01-01 14:56:47 -0500

    Update ClassList.php

--- client/ClassList.php
+++ client/ClassList.php
@@ -16,50 +16,50 @@ $Section = new Section();
 </style>
 </head>
 <body>
-	<div class="container">
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th scope="col">Section ID</th>
-					<th scope="col">Title</th>
-					<th scope="col">Semester</th>
-					<th scope="col">Location</th>
-					<th scope="col">Day</th>
-					<th scope="col">Start</th>
-					<th scope="col">End</th>
-					<th scope="col">Available Spots</th>
-				</tr>
-			</thead>
-			<tbody>
-			<!-- Retrieve all course sections by instructor  -->
-			<?php
-			$teacher = new Teacher();
-			$getTeacher = $teacher->pullTeacherInfo($_SESSION["userID"]);
-			$teacher = $getTeacher->fetch_assoc();
-			//pull course section details by assigned instructor 
-			$classSections = $Section->pullSectionByTeacher($teacher["id"]);
-			if (!empty($classSections)){
-				while($section = $classSections->fetch_assoc()){
-					//get class details associated with current section
-					$class = $Section->pullClassBySection($section['class_id']);
-					$currClass = $class->fetch_assoc();
-					?>
-					<tr>
-						<th scope="row"><?php echo $section['id'];?></th>
-						<td><?php echo $currClass['title'];?></td>
-						<td><?php echo $section['semester'];?></td>
-						<td><?php echo $section['location'];?></td>
-						<td><?php echo $section['day'];?></td>
-						<td><?php echo $section['start_time'];?></td>
-						<td><?php echo $section['end_time'];?></td>
-						<td><?php echo $section['availability'];?></td>
-					</tr>
-			<?php
-				}
+<div class="container">
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th scope="col">Section ID</th>
+				<th scope="col">Title</th>
+				<th scope="col">Semester</th>
+				<th scope="col">Location</th>
+				<th scope="col">Day</th>
+				<th scope="col">Start</th>
+				<th scope="col">End</th>
+				<th scope="col">Available Spots</th>
+			</tr>
+		</thead>
+		<tbody>
+		<!-- Retrieve all course sections by instructor  -->
+		<?php
+		$teacher = new Teacher();
+		$getTeacher = $teacher->pullTeacherInfo($_SESSION["userID"]);
+		$teacher = $getTeacher->fetch_assoc();
+		//pull course section details by assigned instructor 
+		$classSections = $Section->pullSectionByTeacher($teacher["id"]);
+		if (!empty($classSections)){
+			while($section = $classSections->fetch_assoc()){
+			//get class details associated with current section
+			$class = $Section->pullClassBySection($section['class_id']);
+			$currClass = $class->fetch_assoc();
+		?>
+			<tr>
+				<th scope="row"><?php echo $section['id'];?></th>
+				<td><?php echo $currClass['title'];?></td>
+				<td><?php echo $section['semester'];?></td>
+				<td><?php echo $section['location'];?></td>
+				<td><?php echo $section['day'];?></td>
+				<td><?php echo $section['start_time'];?></td>
+				<td><?php echo $section['end_time'];?></td>
+				<td><?php echo $section['availability'];?></td>
+			</tr>
+		<?php
 			}
-			?>
-			</tbody>
-		</table>
-	</div>
+		}
+		?>
+		</tbody>
+	</table>
+</div>
 </body>
 </html>
