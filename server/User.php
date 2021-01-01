a33355820534b29c47db8909ff00ca918f0c3607
lauramurphy12 <55330536+lauramurphy12@users.noreply.github.com>
2021-01-01 14:33:43 -0500

    Update User.php

--- server/User.php
+++ server/User.php
@@ -14,40 +14,40 @@ class User extends dbConnection {
 	
 	function __construct(){
 	}
+	
 	public function setFirstName($par_firstname) {
-        $this->firstname= $par_firstname;
-        return $this;
-    }
+        	$this->firstname= $par_firstname;
+        	return $this;
+        }
 	public function setLastName($par_lastname) {
-        $this->lastname = $par_lastname;
-        return $this;
-    }
+        	$this->lastname = $par_lastname;
+        	return $this;
+   	}
 	public function setAddress($par_address) {
-        $this->address = $par_address;
-        return $this;
-    }
+        	$this->address = $par_address;
+        	return $this;
+    	}
 	public function setPhone($par_phone) {
-        $this->phone = $par_phone;
-        return $this;
-    }
+       		$this->phone = $par_phone;
+        	return $this;
+    	}
 	public function setEmail($par_email) {
-        $this->email = $par_email;
-        return $this;
-    }
-	public function setUsertype($par_usertype) {
-        $this->usertype = $par_usertype;
-        return $this;
-    }
-	public function __toString()
-    {
-        $userInfo = 'First Name: ' . $this->firstname . PHP_EOL;
-        $userInfo .= 'Last Name: ' . $this->lastname . PHP_EOL;
-        $userInfo .= 'Address: ' . $this->address . PHP_EOL;
+        	$this->email = $par_email;
+       		return $this;
+    	}
+	public function setUsertype($par_usertype){
+        	$this->usertype = $par_usertype;
+        	return $this;
+    	}
+	public function __toString(){
+       		$userInfo = 'First Name: ' . $this->firstname . PHP_EOL;
+        	$userInfo .= 'Last Name: ' . $this->lastname . PHP_EOL;
+        	$userInfo .= 'Address: ' . $this->address . PHP_EOL;
 		$userInfo .= 'Phone: ' . $this->phone . PHP_EOL;
 		$userInfo .= 'Email: ' . $this->email . PHP_EOL;
 		$userInfo .= 'Type of User: ' . $this->usertype . PHP_EOL;
-        return $userInfo;
-    }
+        	return $userInfo;
+   	 }
 	
 	function pushUserInfo(){
 		$this->conn = $this->establishConnection();
@@ -59,6 +59,7 @@ class User extends dbConnection {
 		}
 		return $userAdded;
 	}
+	
 	function pullUserInfoByID($userID){
 		$this->conn = $this->establishConnection();
 		$pullUserSQL = $this->conn->prepare("SELECT * FROM User WHERE user_id = ?");
@@ -77,7 +78,6 @@ class User extends dbConnection {
 		$pullUserSQL->execute();
 		$result = $pullUserSQL->get_result();
 		if($result!=null){
-			//return $result->fetch_assoc();
 			return $result;
 		}else{
 			throw new Exception('Could not pull user info');
